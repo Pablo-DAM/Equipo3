30b48c2d81ea7a84bf8d8bf34b3b53244610f962
Adrian de Diego <adrian.dediegofernandez@iesmiguelherrero.com>
2023-05-22 17:34:48 +0200

    commit Adrian

--- prueba2/src/main/java/proyecto/prueba2/Prueba2.java
+++ prueba2/src/main/java/proyecto/prueba2/Prueba2.java
@@ -14,5 +14,6 @@ public class Prueba2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("prueba");
+        System.out.println("Soy Adrian");
     }
 }
